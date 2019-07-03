6e8cf50db1d0f4ee25a9452e84faf0991f9e7a32
dongguoyang <dongguoyang@jd.com>
2019-07-03 11:59:25 +0800

    first commit

--- src/components/Layout/Navigation.js
+++ src/components/Layout/Navigation.js
@@ -1,18 +1,10 @@
 import React, {Component} from 'react';
-import BabelNav from '@jmfe/babel-nav';
-import {deleteCookie} from 'MODULES/pub/cookie';
-import {fetchFormData} from 'MODULES/fetch';
 import * as AppConst from 'CONSTANTS/AppConst';
 import PropTypes from 'prop-types';
 
 
 export default class Babelnav extends Component {
-
-
   render () {
-    const {language, userInfo} = this.props;
-    let userType = userInfo.userType || 0;
-    let erpNo = userInfo.erpNo || '';
     return (
       <div className="layout-left "> 
         <br/>
